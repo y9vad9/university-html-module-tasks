1c2b58e6969401c0a882d00dc8ecf21fa53a4ef3
y9vad9 <y9vad9@gmail.com>
2024-05-18 18:26:54 +0200

    feat: 9th topic is added

---
+++ 9-topic/scripts.js
@@ -0,0 +1,6 @@
+function calculateSum() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 + num2;
+    document.getElementById('result').textContent = 'Сума: ' + result;
+}
